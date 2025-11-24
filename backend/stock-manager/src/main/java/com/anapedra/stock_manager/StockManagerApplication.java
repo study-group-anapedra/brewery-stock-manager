d28b96914c8810c41c691c012c08c604597c2b07
anapedra <anapedra.mil@gmail.com>
2025-11-24 14:07:54 -0300

    feat: Adiciona estrutura inicial do projeto e .gitignore

---
+++ backend/stock-manager/src/main/java/com/anapedra/stock_manager/StockManagerApplication.java
@@ -0,0 +1,13 @@
+package com.anapedra.stock_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagerApplication.class, args);
+	}
+
+}

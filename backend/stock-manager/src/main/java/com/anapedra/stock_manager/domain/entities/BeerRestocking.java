147a0862c15a8b89f726b65fb019715204497eba
anapedra <anapedra.mil@gmail.com>
2025-11-25 15:10:50 -0300

    feat(restocking): Add BeerRestocking entity

---
+++ backend/stock-manager/src/main/java/com/anapedra/stock_manager/domain/entities/BeerRestocking.java
@@ -0,0 +1,96 @@
+package com.anapedra.stock_manager.domain.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_stock_beer")
+public class BeerRestocking implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantity;
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "beer_id", nullable = false)
+    private Beer beer;
+
+    public BeerRestocking() {
+    }
+
+    public BeerRestocking(Long id, Integer quantity, Instant moment, Beer beer) {
+        this.id = id;
+        this.quantity = quantity;
+        this.moment = moment;
+        this.beer = beer;
+    }
+
+
+/*
+    public void restoreStock(int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Quantity must be greater than zero");
+        }
+        Stock stock = getBeer().getStock();
+        if (stock == null) {
+            throw new IllegalStateException("Stock object cannot be null for Beer: " + getBeer().getName());
+        }
+        Integer currentQuantity = stock.getQuantity() != null ? stock.getQuantity() : 0;
+        Integer newQuantity = currentQuantity + quantity;
+        stock.setQuantity(newQuantity);
+    }
+
+ */
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Beer getBeer() {
+        return beer;
+    }
+
+    public void setBeer(Beer beer) {
+        this.beer = beer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BeerRestocking that = (BeerRestocking) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

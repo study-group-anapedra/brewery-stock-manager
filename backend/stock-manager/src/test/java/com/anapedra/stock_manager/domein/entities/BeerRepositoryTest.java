37b4f4a7f0d8ce62d404248d4cfb2ae673c33cac
anapedra <anapedra.mil@gmail.com>
2025-11-25 14:14:07 -0300

    test: update BeerRepositoryTest

--- backend/stock-manager/src/test/java/com/anapedra/stock_manager/domein/entities/BeerRepositoryTest.java
+++ backend/stock-manager/src/test/java/com/anapedra/stock_manager/domein/entities/BeerRepositoryTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.dao.EmptyResultDataAccessException;
+
 import java.time.LocalDate;
 import java.util.Optional;
 

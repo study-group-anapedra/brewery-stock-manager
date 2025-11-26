f724e29264f0ecfd5859d6db068c4480e5502af9
anapedra <anapedra.mil@gmail.com>
2025-11-26 15:21:28 -0300

    refactor(controller): Create base directory for REST Controllers (Resources)

---
+++ backend/stock-manager/src/main/java/com/anapedra/stock_manager/controllers/handlers/FieldMessage.java
@@ -0,0 +1,20 @@
+package com.anapedra.stock_manager.controllers.handlers;
+
+public class FieldMessage {
+
+    private String fieldName;
+    private String message;
+
+    public FieldMessage(String fieldName, String message) {
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

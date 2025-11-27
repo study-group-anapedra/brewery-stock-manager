518a1ae8b2aa8a10d7a3d1649ad00f21e5a2a44b
anapedra <anapedra.mil@gmail.com>
2025-11-27 13:10:10 -0300

    feat(api): Add UserController with /me endpoint

---
+++ backend/stock-manager/src/main/java/com/anapedra/stock_manager/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.anapedra.stock_manager.controllers;
+
+import com.anapedra.stock_manager.domain.dtos.UserDTO;
+import com.anapedra.stock_manager.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @GetMapping(value = "/me")
+    public ResponseEntity<UserDTO> getMe() {
+    	UserDTO dto = service.getMe();
+        return ResponseEntity.ok(dto);
+    }
+}
